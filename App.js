770ee0b01ec24a7d1cdbbe94fff97fc1149525df
gopal kumar <kumar.gopal150891@gmail.com>
2025-12-30 19:13:54 +0530

    epidode 2

--- App.js
+++ App.js
@@ -18,23 +18,36 @@
     
 </div> */
 
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading1" }, "hello world from React!"),
-    React.createElement("h1", { id: "heading-label1" }, "Sibling heading"),
+  React.createElement("div", { id: "child1", key: "1" }, [
+    React.createElement(
+      "h1",
+      { id: "heading1", key: 1 },
+      "hello world from React!"
+    ),
+    React.createElement(
+      "h1",
+      { id: "heading-label1", key: 2 },
+      "Sibling heading"
+    ),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading1" }, "hello world from React!"),
-    React.createElement("h1", { id: "heading-label1" }, "Sibling heading"),
+  React.createElement("div", { id: "child2", key: 2 }, [
+    React.createElement(
+      "h1",
+      { id: "heading2", key: 1 },
+      "hello world from React!"
+    ),
+    React.createElement(
+      "h1",
+      { id: "heading-label2", key: 2 },
+      "Sibling headin243223oppakm"
+    ),
   ]),
 ]);
 
-// JSX
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React"
-);
 console.log(parent);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Namste react
+
+#parcel
+
+- dev build
+  -local server
+  -HMR hot module replacement
+  -caching
+  -Image optimization
+  -minification
+  -bundling-
+  compression
+  -Differentila bundling
+  to support old browser. diffrent bundle for diffrent browser.
+  -Dignostics
+  -Error handling
+  - tree shaking code - Remove unused code
+  - diffrent dev and prod bundle
